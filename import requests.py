eeca6dc8e6908c1553344766984c022bc31244de
Matej <matej.lovsin1@gmail.com>
2026-08-29 14:21:11 +0200

    izluscenje sifre in naslova oglasa

--- import requests.py
+++ import requests.py
@@ -1,13 +1,15 @@
-with open("podatki/audi_stran1.html", encoding="cp1250") as f:
+with open("podatki/mercedes_stran1.html", encoding="cp1250") as f:
     html = f.read()
 
 import re
 import bs4
 juha = bs4.BeautifulSoup(html, 'html.parser')
 
-for povezava in juha.find_all('a'):
+for oglas in juha.find_all('div', class_="row bg-white position-relative GO-Results-Row GO-Shadow-B"):
+    povezava = oglas.find('a')
     url = povezava.get('href')
-    if 'details.asp?' in url:
-        ime = povezava.string
+    if '&display' in url:
         id = int(re.search(r'\d{8}', url).group(0))
-        print({'id': id, 'ime': ime})
+    naslov = oglas.find('div', class_="GO-Results-Naziv bg-dark px-3 py-2 font-weight-bold text-truncate text-white text-decoration-none")
+    ime = naslov.find('span').get_text()
+    print({'id': id, 'ime': ime})
